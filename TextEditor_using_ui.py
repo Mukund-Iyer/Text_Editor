b6768aa7824f34589e653f34e0df7c4ca266c182
Mukund-Iyer <mukundiyer1995@gmail.com>
2025-05-20 15:47:36 +0530

    Added "Save" feature that saves changes to an existing or open text files. Additionally, I have also ensured that any file that is currently open, will be show it's full path in the Window title.

--- TextEditor_using_ui.py
+++ TextEditor_using_ui.py
@@ -6,6 +6,7 @@ import webbrowser
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
+        self.loaded_file = ""
         uic.loadUi("untitled.ui", self)  # Load the .ui file
         self.setWindowTitle("Text Editor")
         self.OpenMenuItem.triggered.connect(self.open_file)
@@ -13,6 +14,7 @@ class MyWindow(QtWidgets.QMainWindow):
         self.Save_AsMenuItem.triggered.connect(self.save_as)
         self.NewMenuItem.triggered.connect(self.new_file)
         self.Help_MenuItem.triggered.connect(self.open_help)
+        self.SaveMenuItem.triggered.connect(self.save)
 
     def exit_application(self):
         # Define what happens when ExitMenuItem is triggered
@@ -25,13 +27,25 @@ class MyWindow(QtWidgets.QMainWindow):
 
         if file_name:
             # Get the text from plainTextEdit
-            text = self.ui.plainTextEdit.toPlainText()
+            text = self.plainTextEdit.toPlainText()
             # Write the text to the chosen file
             with open(file_name, 'w') as file:
                 file.write(text)
+                self.loaded_file = file_name
+                self.setWindowTitle("Text Editor - " + file_name)
+
+    def save(self):
+        if self.loaded_file != "" and self.loaded_file:
+            text = self.plainTextEdit.toPlainText()
+            with open(self.loaded_file,'w') as file:
+                file.write(text)
+        else:
+            self.save_as()
 
     def new_file(self):
-        self.ui.plainTextEdit.clear()
+        self.plainTextEdit.clear()
+        self.setWindowTitle("Text Editor")
+        self.loaded_file = ""
 
     def open_file(self):
         option = QFileDialog.Options()
@@ -40,6 +54,8 @@ class MyWindow(QtWidgets.QMainWindow):
             with open(file_name, 'r') as file:
                 text = file.read()
                 self.plainTextEdit.setPlainText(text)
+                self.setWindowTitle("Text Editor - " + file_name)
+                self.loaded_file = file_name
 
     def open_help(self):
         site_url = "https://github.com/Mukund-Iyer/Text_Editor/tree/Text_Editor_Only"
